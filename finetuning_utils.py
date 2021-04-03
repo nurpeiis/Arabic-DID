112331f6b4aa939a661331bafa17088775c5c465
nurpeiis <nb2577@nyu.edu>
2021-04-03 16:39:52 +0400

    Working training

--- finetuning_utils.py
+++ finetuning_utils.py
@@ -1,4 +1,7 @@
-
+import copy
+import time
+import torch
+import numpy as np
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
@@ -19,9 +22,161 @@ def compute_metrics(eval_pred):
     accuracy = accuracy_score(labels, preds)
     return {'accuracy': accuracy, 'precision': precision, 'recall': recall, 'f1': f1, }
 
+# Function to calculate the accuracy of our predictions vs labels
+
+
+def metrics(preds, labels, total_metrics):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    precision, recall, f1, _ = precision_recall_fscore_support(
+        labels_flat, pred_flat, average='weighted', pos_label=1)
+    accuracy = accuracy_score(labels_flat, pred_flat)
+    list_metrics = ['accuracy', 'precision', 'recall', 'f1']
+    cur_metrics = {'accuracy': accuracy,
+                   'precision': precision, 'recall': recall, 'f1': f1}
+    for m in list_metrics:
+        total_metrics[m] += cur_metrics[m]
+    return cur_metrics
+
 
 def cost_function(cost_dict):
     return cost_dict['eval_loss']
+
+
+def train(model, train_dataloader, cross_entropy, optimizer, device):
+
+    model.train()
+
+    total_loss = 0
+    list_metrics = ['accuracy', 'precision', 'recall', 'f1']
+    total_metrics = {}
+    for m in list_metrics:
+        total_metrics[m] = 0
+    # empty list to save model predictions
+    total_preds = []
+
+    # iterate over batches
+    for step, batch in enumerate(train_dataloader):
+
+        # progress update after every 50 batches.
+        if step % 50 == 0 and not step == 0:
+            print('  Batch {:>5,}  of  {:>5,}.'.format(
+                step, len(train_dataloader)))
+
+        # push the batch to gpu
+        #batch = [r.to(device) for r in batch]
+
+        input_ids = batch['input_ids'].to(device)
+        mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        # clear previously calculated gradients
+        model.zero_grad()
+
+        # get model predictions for the current batch
+        preds = model(input_ids, mask)
+
+        # compute the loss between actual and predicted values
+        loss = cross_entropy(preds, labels)
+
+        # add on to the total loss
+        total_loss = total_loss + loss.item()
+
+        # backward pass to calculate the gradients
+        loss.backward()
+
+        # clip the the gradients to 1.0. It helps in preventing the exploding gradient problem
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+        # update parameters
+        optimizer.step()
+
+        # model predictions are stored on GPU. So, push it to CPU
+        preds_a = preds.detach().cpu().numpy()
+        label_ids = labels.to('cpu').numpy()
+
+        # Calculate the accuracy for this batch of test sentences.
+        tmp_metrics = metrics(preds_a, label_ids,  total_metrics)
+        #print(f'Tmp metrics {tmp_metrics}%')
+
+        # append the model predictions
+        total_preds.append(preds_a)
+
+    # compute the training loss of the epoch
+    avg_loss = total_loss / len(train_dataloader)
+    avg_metrics = {}
+    for m in list_metrics:
+        avg_metrics[m] = total_metrics[m] / len(train_dataloader)
+    # predictions are in the form of (no. of batches, size of batch, no. of classes).
+    # reshape the predictions in form of (number of samples, no. of classes)
+    total_preds = np.concatenate(total_preds, axis=0)
+
+    # returns the loss and predictions
+    return avg_loss, total_preds, avg_metrics
+# function for evaluating the model
+
+
+def evaluate(model, dev_dataloader, cross_entropy, device):
+
+    print("\nEvaluating...")
+
+    # deactivate dropout layers
+    model.eval()
+
+    total_loss = 0
+    list_metrics = ['accuracy', 'precision', 'recall', 'f1']
+    total_metrics = {}
+    for m in list_metrics:
+        total_metrics[m] = 0
+
+    # empty list to save the model predictions
+    total_preds = []
+
+    # iterate over batches
+    for step, batch in enumerate(dev_dataloader):
+
+        # Progress update every 50 batches.
+        if step % 50 == 0 and not step == 0:
+
+            # Calculate elapsed time in minutes.
+            #elapsed = format_time(time.time() - t0)
+
+            # Report progress.
+            print('  Batch {:>5,}  of  {:>5,}.'.format(
+                step, len(dev_dataloader)))
+
+        # push the batch to gpu
+        input_ids = batch['input_ids'].to(device)
+        mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+
+        # deactivate autograd
+        with torch.no_grad():
+
+            # model predictions
+            preds = model(input_ids, mask)
+
+            # compute the validation loss between actual and predicted values
+            loss = cross_entropy(preds, labels)
+
+            total_loss = total_loss + loss.item()
+
+            preds_a = preds.detach().cpu().numpy()
+            label_ids = labels.to('cpu').numpy()
+
+            # Calculate the accuracy for this batch of test sentences.
+            tmp_metrics = metrics(preds_a, label_ids,  total_metrics)
+            print(f'Tmp metrics {tmp_metrics}%')
+        total_preds.append(preds)
+
+    # compute the validation loss of the epoch
+    avg_loss = total_loss / len(dev_dataloader)
+    avg_metrics = {}
+    for m in list_metrics:
+        avg_metrics[m] = total_metrics[m] / len(dev_dataloader)
+    # reshape the predictions in form of (number of samples, no. of classes)
+    total_preds = np.concatenate(total_preds, axis=0)
+
+    return avg_loss, total_preds, avg_metrics
 
 
 def model_init():
--- run_training.py
+++ run_training.py
@@ -7,32 +7,44 @@ import argparse
 import did_dataset
 import data_utils
 import finetuning_utils
-import json
-import ray
 import pandas as pd
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 
-
-from sklearn.model_selection import train_test_split
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AdamW
 from transformers import TrainingArguments, Trainer
-
+from classifier import Classifier
+"""
 from ray import tune
 from ray.tune.schedulers import AsyncHyperBandScheduler
 from ray.tune.suggest import ConcurrencyLimiter
 from ray.tune.suggest.bayesopt import BayesOptSearch
+"""
 
 parser = argparse.ArgumentParser(
-    description="Run a hyperparameter search for finetuning a RoBERTa model on the BoolQ dataset."
+    description="Run a hyperparameter search for finetuning a BERT models for Arabic DID."
 )
 parser.add_argument(
-    "data_dir",
+    "level",
     type=str,
-    help="Directory containing the BoolQ dataset. Can be downloaded from https://dl.fbaipublicfiles.com/glue/superglue/data/v2/BoolQ.zip.",
+    help="Level of dialect, i.e. city, country, region",
 )
 
 args = parser.parse_args()
+level = args.level
+train_batch_size = 8
+val_batch_size = 32
+test_batch_size = 32
+max_seq_length = 64
+dropout_prob = 0.1
+hidden_size1 = 768
+hidden_size2 = 512
+num_classes = 26
+epochs = 3
+learning_rate = 1e-5
 
-
+print('Getting data')
 folder = '../hierarchical-did/data_processed_second/madar_shared_task1/'
 train_df = data_utils.get_df_from_files(
     [f'{folder}MADAR-Corpus-26-train.lines', f'{folder}MADAR-Corpus-6-train.lines'])
@@ -41,44 +53,65 @@ test_df = data_utils.get_df_from_files(
 val_df = data_utils.get_df_from_files(
     [f'{folder}MADAR-Corpus-26-dev.lines', f'{folder}MADAR-Corpus-6-dev.lines'])
 
-
+print('Loading tokenizer')
 tokenizer = AutoTokenizer.from_pretrained(
     'CAMeL-Lab/bert-base-camelbert-mix')
-train_data = did_dataset.DIDDataset(train_df, tokenizer)
-val_data = did_dataset.DIDDataset(val_df, tokenizer)
-test_data = did_dataset.DIDDataset(test_df, tokenizer)
+print('In data utils Dataset object')
+train_data = did_dataset.DIDDataset(
+    train_df, tokenizer, level, 'labels/madar_labels_city.txt', max_seq_length)
+val_data = did_dataset.DIDDataset(
+    val_df, tokenizer, level, 'labels/madar_labels_city.txt', max_seq_length)
+test_data = did_dataset.DIDDataset(
+    test_df, tokenizer, level, 'labels/madar_labels_city.txt', max_seq_length)
 
 
-training_args = TrainingArguments(
-    output_dir="./models/",
-    do_train=True,
-    do_eval=True,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=64,
-    num_train_epochs=3,  # due to time/computation constraints
-    evaluation_strategy="epoch",
-)
-trainer = Trainer(
-    model_init=finetuning_utils.model_init,
-    args=training_args,
-    train_dataset=train_data,
-    eval_dataset=val_data,
-    tokenizer=tokenizer,
-    compute_metrics=finetuning_utils.compute_metrics
-)
-trainer.train()
-"""
-best_trial = trainer.hyperparameter_search(
-    hp_space=lambda _: {
-        "learning_rate": tune.uniform(1e-5, 5e-5),
-    },
-    direction="maximize",
-    backend="ray",
-    n_trials=5,  # number of hyperparameter samples
-    compute_objective=lambda x: x['eval_loss']
-)
+# dataLoader for train set
+train_dataloader = DataLoader(
+    train_data, shuffle=True, batch_size=train_batch_size, num_workers=0)
+val_dataloader = DataLoader(
+    val_data, shuffle=True, batch_size=val_batch_size, num_workers=0)
+test_dataloader = DataLoader(
+    test_data, batch_size=test_batch_size, num_workers=0)
 
-print(f'Run ID: {best_trial[0]}')
-print(f'Objective: {best_trial[1]}')
-print(f'Hyperparameters: {best_trial[2]}')
-"""
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f'{device} device for training')
+bert = finetuning_utils.model_init()
+model = Classifier(bert, dropout_prob, hidden_size1, hidden_size2, num_classes)
+model.to(device)
+
+# optimizer from hugging face transformers
+
+# define the optimizer
+optimizer = AdamW(model.parameters(),
+                  lr=learning_rate)
+cross_entropy = nn.NLLLoss()
+
+
+best_valid_loss = float('inf')
+
+# empty lists to store training and validation loss of each epoch
+train_losses = []
+valid_losses = []
+
+# for each epoch
+for epoch in range(epochs):
+    print(f'Epoch {epoch+1}/{epochs}')
+    # train model
+    train_loss, _, train_metrics = finetuning_utils.train(model, train_dataloader,
+                                                          cross_entropy, optimizer, device)
+
+    # evaluate model
+    valid_loss, _, valid_metrics = finetuning_utils.evaluate(model, val_dataloader,
+                                                             cross_entropy, device)
+
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'saved_weights.pt')
+
+    train_losses.append(train_loss)
+    valid_losses.append(valid_loss)
+
+    print(
+        f'Training Loss: {train_loss:.3f}, Training Metrics: {train_metrics}')
+    print(
+        f'Validation Loss: {valid_loss:.3f}, Validation Metrics: {valid_metrics}')

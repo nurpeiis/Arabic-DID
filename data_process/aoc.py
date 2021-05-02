f4c7a7f67bc6377d3926fad54c71224df4debb8d
Alem <ans682@nyu.edu>
2021-05-02 23:12:55 +0400

    add aoc.py

---
+++ data_process/aoc.py
@@ -0,0 +1,73 @@
+from data_process import DataProcess
+
+# Specify a directory to the dataset that you want to preprocess.
+<<<<<<< HEAD
+dp = DataProcess('../aoc/', 'mechanical_turk_annotators', 'news_comments',
+=======
+dp = DataProcess('/Users/alemshaimardanov/PycharmProjects/NLP/arabic_did/data_processed_check/aoc/', 'mechanical_turk_annotators', 'news_comments',
+>>>>>>> 39c40a32abb419c3292435fd3a80777e4d1b42d4
+                 'https://www.cis.upenn.edu/~ccb/data/AOC-dialectal-annotations.zip', 'aoc', {}, {}, 0, 'corpus', 'manual')
+
+if dp.split_original_manual == 'manual':
+    # Specify a directory to unprocessed dataset which stores AOC-dialectal-annotations.
+    dp.save_file('dialect_alghad-segs.tsv', dp.preprocess(
+<<<<<<< HEAD
+        '../AOC-dialectal-annotations/dialect_alghad-segs.txt.norm', '', '', '', 'levant', header=None))
+
+    # Specify a directory to unprocessed dataset which stores AOC-dialectal-annotations.
+    dp.save_file('dialect_youm7-segs.tsv', dp.preprocess(
+        '../AOC-dialectal-annotations/dialect_youm7-segs.txt.norm', '', '', 'eg', 'nile_basin', header=None))
+
+    # Specify a directory to unprocessed dataset which stores AOC-dialectal-annotations.
+    dp.save_file('dialect_alriyadh-segs.tsv', dp.preprocess(
+        '../AOC-dialectal-annotations/dialect_alriyadh-segs.txt.norm', '', '', '', 'gulf', header=None))
+
+    # Specify a directory to unprocessed dataset which stores AOC-dialectal-annotations.
+    dp.save_file('MSA_alghad-segs.tsv', dp.preprocess(
+        '../AOC-dialectal-annotations/MSA_alghad-segs.txt.norm', 'msa', 'msa', 'msa', 'msa', header=None))
+
+    # Specify a directory to unprocessed dataset which stores AOC-dialectal-annotations.
+    dp.save_file('MSA_youm7-segs.tsv', dp.preprocess('../AOC-dialectal-annotations/MSA_youm7-segs.txt.norm',
+=======
+        '/Users/alemshaimardanov/PycharmProjects/NLP/arabic_did/data_raw_main/AOC-dialectal-annotations/dialect_alghad-segs.txt.norm', '', '', '', 'levant', header=None))
+
+    # Specify a directory to unprocessed dataset which stores AOC-dialectal-annotations.
+    dp.save_file('dialect_youm7-segs.tsv', dp.preprocess(
+        '/Users/alemshaimardanov/PycharmProjects/NLP/arabic_did/data_raw_main/AOC-dialectal-annotations/dialect_youm7-segs.txt.norm', '', '', 'eg', 'nile_basin', header=None))
+
+    # Specify a directory to unprocessed dataset which stores AOC-dialectal-annotations.
+    dp.save_file('dialect_alriyadh-segs.tsv', dp.preprocess(
+        '/Users/alemshaimardanov/PycharmProjects/NLP/arabic_did/data_raw_main/AOC-dialectal-annotations/dialect_alriyadh-segs.txt.norm', '', '', '', 'gulf', header=None))
+
+    # Specify a directory to unprocessed dataset which stores AOC-dialectal-annotations.
+    dp.save_file('MSA_alghad-segs.tsv', dp.preprocess(
+        '/Users/alemshaimardanov/PycharmProjects/NLP/arabic_did/data_raw_main/AOC-dialectal-annotations/MSA_alghad-segs.txt.norm', 'msa', 'msa', 'msa', 'msa', header=None))
+
+    # Specify a directory to unprocessed dataset which stores AOC-dialectal-annotations.
+    dp.save_file('MSA_youm7-segs.tsv', dp.preprocess('/Users/alemshaimardanov/PycharmProjects/NLP/arabic_did/data_raw_main/AOC-dialectal-annotations/MSA_youm7-segs.txt.norm',
+>>>>>>> 39c40a32abb419c3292435fd3a80777e4d1b42d4
+                 'msa', 'msa', 'msa', 'msa', header=None))
+
+    # Specify a directory to unprocessed dataset which stores AOC-dialectal-annotations.
+    dp.save_file('MSA_alriyadh-segs.tsv', dp.preprocess(
+<<<<<<< HEAD
+        '../AOC-dialectal-annotations/MSA_alriyadh-segs.txt.norm', 'msa', 'msa', 'msa', 'msa', header=None))
+=======
+        '/Users/alemshaimardanov/PycharmProjects/NLP/arabic_did/data_raw_main/AOC-dialectal-annotations/MSA_alriyadh-segs.txt.norm', 'msa', 'msa', 'msa', 'msa', header=None))
+>>>>>>> 39c40a32abb419c3292435fd3a80777e4d1b42d4
+
+else:
+    files = ['dialect_alghad-segs.tsv', 'dialect_youm7-segs.tsv', 'dialect_alriyadh-segs.tsv',
+         'MSA_alghad-segs.tsv', 'MSA_youm7-segs.tsv', 'MSA_alriyadh-segs.tsv']
+    for file in files:
+        df_train, df_dev, df_test = dp.split(file, 0.8, 0.1, 0.1)
+        dp.save_file('train_'+file, df_train)
+        dp.save_file('dev_'+file, df_dev)
+        dp.save_file('test_'+file, df_test)
+    
+# Specify a directory where you want to save datasets_splitted_features file.
+<<<<<<< HEAD
+dp.save_features('../datasets_splited_features.tsv')
+=======
+dp.save_features('/Users/alemshaimardanov/PycharmProjects/NLP/arabic_did/datasets_splited_features.tsv')
+>>>>>>> 39c40a32abb419c3292435fd3a80777e4d1b42d4
